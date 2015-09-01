59384a0342a1e8057d9f3aa84732e91e22c8c04c
clark173 <clark173@purdue.edu>
2015-09-01 10:52:19 -0400

    Added ability to read from files in test folder.

--- src/Micro.java
+++ src/Micro.java
@@ -1,8 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class Micro {
 
-    public static void main(String[] args)
-    {
+    public void readFiles(String filename) {
+        //System.out.println(filename);
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filename), Charset.defaultCharset());
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getTestFiles() {
+        String test_directory = System.getProperty("user.dir") + "/tests/";
+        File directory = new File(test_directory);
+        File[] fileList = directory.listFiles();
+
+        for (File file : fileList) {
+            if (file.isFile()) {
+                readFiles(directory + "/" + file.getName());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Micro micro = new Micro();
+        micro.getTestFiles();
         //TODO: Initiate execution
+        //MicroLexer lexer = new MicroLexer(new ANTLRInputStream());
+        //TokenList tokens = new Micro(lexer);
     }
 
 }
