5356b45e85527f12c19b01a31aa2bb221f018b7b
clark173 <clark173@purdue.edu>
2015-09-01 20:56:09 -0400

    stuff in progress

--- src/Micro.java
+++ src/Micro.java
@@ -4,25 +4,44 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
 
 
 public class Micro {
 
-    public void parseFile(List<String> lines) {
+    public void parseFile(String lines) {
+        MicroLexer lexer = new MicroLexer(new ANTLRInputStream(lines));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MicroParser parser = new MicroParser(tokens);
         
     }
 
     public void readFiles(String filename) {
-        String absolute_filename = System.getProperty("user.dir") + "/tests/" + filename;
         try {
             List<String> lines = Files.readAllLines(Paths.get(filename), Charset.defaultCharset());
-            parseFile(lines);
+            String lineString = "";
+            for (String line : lines) {
+                lineString += line + "\n";
+            }
+            parseFile(lineString);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        MicroLexer lexer = new MicroLexer(new ANTLRFileStream(args[0]));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        for (Object o : tokens.getTokens()){
+            CommonToken token = (CommonToken)o;
+            System.out.println("token: " + token.getText());
+        }    
+    
+        System.out.println(tokens.getTokens());
         Micro micro = new Micro();
         micro.readFiles(args[0]);
     }
