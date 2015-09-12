146b622611d0f2a2764855ddebc4417a40f34878
clark173 <clark173@purdue.edu>
2015-09-11 20:14:31 -0400

    Removed the step 2 code and added reference to parser.

--- src/Micro.java
+++ src/Micro.java
@@ -13,48 +13,17 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 
 public class Micro {
-    public static final int COMMENT = 2;
-    public static final int KEYWORD = 3;
-    public static final int IDENTIFIER = 4;
-    public static final int INTLITERAL = 5;
-    public static final int OPERATOR = 6;
-    public static final int WHITESPACE = 7;
-    public static final int STRINGLITERAL = 8;
-    public static final int FLOAT = 9;
-
-    public static void printTokens(Token token) {
-        String tokenType = "";
-        switch(token.getType()) {
-            case COMMENT : tokenType = "COMMENT";
-                 break;
-            case KEYWORD : tokenType = "KEYWORD";
-                 break;
-            case IDENTIFIER : tokenType = "IDENTIFIER";
-                 break;
-            case INTLITERAL : tokenType = "INTLITERAL";
-                 break;
-            case OPERATOR : tokenType = "OPERATOR";
-                 break;
-            case WHITESPACE : tokenType = "WHITESPACE";
-                 break;
-            case STRINGLITERAL : tokenType = "STRINGLITERAL";
-                 break;
-            case FLOAT : tokenType = "FLOATLITERAL";
-                 break;
-        }
-        System.out.println("Token Type: " + tokenType);
-        System.out.println("Value: " + token.getText());
-    }
 
     public static void main(String[] args) throws Exception {
         MicroLexer lexer = new MicroLexer(new ANTLRFileStream(args[0]));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         tokens.fill();
-        for (Token token : tokens.getTokens()) {
-            if (token.getType() != -1) {
-                printTokens(token);
-            }
-        }
+        MicroParser parser = new MicroParser(tokens);
+
+        //ANTLRErrorStrategy es = new CustomErrorStrategy();
+        //parser.setErrorHandler(es);
+
+        System.out.println("Accepted");
     }
 
 }
