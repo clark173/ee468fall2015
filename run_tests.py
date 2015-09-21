966c061006132baf9c6b42a172ec351de847e0f8
clark173 <clark173@purdue.edu>
2015-09-21 18:16:54 -0400

    Modified the test script.

--- run_tests.py
+++ run_tests.py
@@ -1,39 +1,16 @@
-import glob
 import os
-import sys
 
 
 IN_PATH = 'testcases/input/'
 OUT_PATH = 'testcases/output/'
+VALID_FILES = ['1', '5', '6', '7', '8', '9', '11', '13',
+               '14', '16', '18', '19', '20', '21']
 
 
-try:
-    os.system('make all')
-except:
-    print 'Error compiling project. Exiting...'
-    sys.exit(1)
-input_files = []
-for infile in os.listdir(IN_PATH):
-    if infile[0] != '.':
-        input_files.append(infile)
+os.system('make all')
 
-input_files = sorted(input_files)
-new_filelist = []
-new_filelist.append(input_files[0])
-new_filelist.append(input_files[11])
-new_filelist.append(input_files[15])
-
-for i in range(16, len(input_files)):
-    new_filelist.append(input_files[i])
-for i in range(1,11):
-    new_filelist.append(input_files[i])
-for i in range(12, 14):
-    new_filelist.append(input_files[i])
-
-test_num = 1
-for filename in new_filelist:
-    print 'Running test %s' %str(test_num)
-    os.system('java -cp lib/antlr.jar:classes/ Micro %s%s > test.out\
+for num in VALID_FILES:
+    print '\nRunning test %s' %num
+    os.system('java -cp lib/antlr.jar:classes/ Micro %stest%s.micro > test.out\
         && diff -b -B test.out %stest%s.out'
-        %(IN_PATH, filename, OUT_PATH, str(test_num)))
-    test_num += 1
+        %(IN_PATH, num, OUT_PATH, num))
